b607dcd20408b70ff3a3c28983aa0d3a375477a5
ssiddarthsai1999 <ssiddarthsai1999@gmail.com>
2023-03-12 21:12:41 +0530

    testimonialsResponsive

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import TestimonialsCard from "./components/TestimonialsCard/TestimonialsCard";
 import Testimonials from "./pages/Testimonials/Testimonials";
 
 function App() {
-    return <div className="App">
-     <TestimonialsCard/>
-    </div>;
+    return (
+        <div className="App">
+            <Testimonials />
+        </div>
+    );
 }
 
 export default App;
--- src/pages/Testimonials/Testimonials.js
+++ src/pages/Testimonials/Testimonials.js
@@ -10,11 +10,44 @@ function Testimonials() {
             author: "Donald Trump",
             work: "President @ United States",
         },
+
+        {
+            review: "I've been using this product from AvantGarde and it's been a game-changer for me. It's secure, easy to use, and offers lightning-fast transactions. I'm impressed with their innovative approach to the digital asset space.",
+            img: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+            author: "Donald Trump",
+            work: "President @ United States",
+        },
+
+        {
+            review: "I've been using this product from AvantGarde and it's been a game-changer for me. It's secure, easy to use, and offers lightning-fast transactions. I'm impressed with their innovative approach to the digital asset space.",
+            img: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+            author: "Donald Trump",
+            work: "President @ United States",
+        },
+
+        {
+            review: "I've been using this product from AvantGarde and it's been a game-changer for me. It's secure, easy to use, and offers lightning-fast transactions. I'm impressed with their innovative approach to the digital asset space.",
+            img: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+            author: "Donald Trump",
+            work: "President @ United States",
+        },
+
+        {
+            review: "I've been using this product from AvantGarde and it's been a game-changer for me. It's secure, easy to use, and offers lightning-fast transactions. I'm impressed with their innovative approach to the digital asset space.",
+            img: "https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg",
+            author: "Donald Trump",
+            work: "President @ United States",
+        },
     ];
 
     return (
-        <div>
-            <div class="testimonial-container">
+        <div className="test">
+            <div className="testimonials-full">
+                {" "}
+                <h1>Testimonials</h1>
+                <h2>What our clients say about us</h2>
+            </div>
+            <div class="testimonials-container">
                 {data.map((item) => {
                     console.log(`item is ${item}`);
                     return (
