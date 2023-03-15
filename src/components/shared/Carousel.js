02f40e95f58d069db60112cc83c7a55b53b2c528
Sujin K <lksujins@gmail.com>
2023-03-15 15:56:56 +0530

    clients final

--- src/components/shared/Carousel.js
+++ src/components/shared/Carousel.js
@@ -1,26 +1,18 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ReactComponent as Arrow } from "../../assets/carousel-arrow.svg";
 
-const Carousel = () => {
+const Carousel = ({ clients }) => {
     const itemsRef = useRef({});
 
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+    const step = 1;
 
     const showingIndex = useRef(0);
+    const [reset, setReset] = useState(0);
 
     useEffect(() => {
         const timer = setInterval(() => {
-            itemsRef.current?.[showingIndex.current]?.classList.remove(
-                "bg-red-800"
-            );
-            itemsRef.current?.[showingIndex.current]?.classList.add(
-                "bg-green-800"
-            );
-            showingIndex.current = (showingIndex.current + 1) % arr.length;
-            itemsRef.current?.[showingIndex.current]?.classList.add(
-                "bg-red-800"
-            );
-
+            const nextIndex = (showingIndex.current + step) % clients.length;
+            showingIndex.current = nextIndex;
             itemsRef.current?.[showingIndex.current]?.scrollIntoView({
                 behavior: "smooth",
                 block: "center",
@@ -29,26 +21,54 @@ const Carousel = () => {
         return () => {
             clearInterval(timer);
         };
-    }, []);
+    }, [reset]);
+
+    const onNextClick = () => {
+        const nextIndex = (showingIndex.current + step) % clients.length;
+        showingIndex.current = nextIndex;
+        itemsRef.current?.[showingIndex.current]?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+        });
+        setReset(reset + 1);
+    };
+
+    const onPrevClick = () => {
+        const prevIndex =
+            showingIndex.current === 0
+                ? clients.length - 1
+                : showingIndex.current - 1;
+        showingIndex.current = prevIndex;
+        itemsRef.current?.[showingIndex.current]?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+        });
+        setReset(reset + 1);
+    };
 
     return (
-        <div className=" flex flex-1 gap-5 items-center ">
-            <Arrow className="w-20" />
-            <div className=" flex-1 flex gap-20 items-center overflow-x-scroll carousel">
-                {arr.map((index) => (
-                    <div
+        <div className=" flex flex-1 gap-5 items-center select-none">
+            <Arrow
+                className="w-12 sm:w-20 cursor-pointer transition-all hover:scale-110 transform"
+                onClick={onPrevClick}
+            />
+            <div className=" flex-1 flex sm:gap-20 gap-8 items-center overflow-x-scroll carousel">
+                {clients.map(({ name, image }, index) => (
+                    <img
+                        src={image}
+                        alt={name}
                         key={index}
                         ref={(ref) => {
                             itemsRef.current[index - 1] = ref;
                         }}
-                        style={{
-                            minWidth: 160,
-                        }}
-                        className="  w-40 h-40 bg-green-800"
-                    ></div>
+                        className=" sm:h-40 h-32"
+                    />
                 ))}
             </div>
-            <Arrow className="w-20 rotate-180" />
+            <Arrow
+                className="w-12 sm:w-20 rotate-180 cursor-pointer transition-all hover:scale-110 transform"
+                onClick={onNextClick}
+            />
         </div>
     );
 };
--- src/pages/Clients/Clients.js
+++ src/pages/Clients/Clients.js
@@ -3,12 +3,48 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Carousel from "../../components/shared/Carousel";
 
 const Clients = () => {
+    const clients = [
+        {
+            name: "Mushrohms NFT",
+            image: require("../../assets/images/Mushrohms.png"),
+        },
+        {
+            name: "Cryption Network",
+            image: require("../../assets/images/9747.png"),
+        },
+        {
+            name: "YFDAI Finance",
+            image: require("../../assets/images/YfdaiFinance.png"),
+        },
+
+        {
+            name: "Shiba Inu Token",
+            image: require("../../assets/images/ShibaInuToken.png"),
+        },
+        {
+            name: "Milkshake Finance",
+            image: require("../../assets/images/MilkshakeFinance.png"),
+        },
+        {
+            name: "Emillions NFT",
+            image: require("../../assets/images/EmillionsNFT.png"),
+        },
+        {
+            name: "Galaxia Protocol",
+            image: require("../../assets/images/Galaxia.png"),
+        },
+        {
+            name: "Galaxia Protocol",
+            image: require("../../assets/images/Galaxia.png"),
+        },
+    ];
+
     return (
         <div className=" w-full h-full flex flex-col items-center p-3">
             <SectionHeader />
 
-            <div className=" w-full max-w-7xl flex-1 flex flex-col">
-                <Carousel />
+            <div className=" w-full max-w-7xl flex-1 mt-20">
+                <Carousel clients={clients} />
             </div>
         </div>
     );
